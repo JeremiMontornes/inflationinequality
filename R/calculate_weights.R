ed7995eb6b05c050ffd155b555880156ada96b64
Pradana AUMARS <Pradana.AUMARS@banque-france.fr>
2024-06-25 16:34:36 +0200

    perf: remove useless column

--- R/calculate_weights.R
+++ R/calculate_weights.R
@@ -162,6 +162,7 @@ calculate_weights <- function(country, category, level = 2,
 
   # Remove intermediate columns to reduce memory usage
   dt_weighted_consumption[, `:=`(
+    weight = NULL,
     consumption = NULL,
     preweighted_consumption = NULL,
     total_consumption = NULL,
