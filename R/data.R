794e55ff68e3dfa4cc3478428286a3aaee54c1d0
Pradana AUMARS <Pradana.AUMARS@banque-france.fr>
2024-08-09 11:40:46 +0200

    docs: fix COICOP_2018 documentation

--- R/data.R
+++ R/data.R
@@ -3,12 +3,10 @@
 #' The complete 2018 edition of the Classification of Individual Consumption
 #' According to Purpose structure
 #'
-#' @format ## `COICOP_2018`
-#' A data.table with 871 rows and 2 columns:
+#' @format
 #' \describe{
 #'   \item{code}{COICOP code}
 #'   \item{title}{title of product category}
-#'   ...
 #' }
 #' @source <https://unstats.un.org/unsd/classifications/econ/>
 "COICOP_2018"
